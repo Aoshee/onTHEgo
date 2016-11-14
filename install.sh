0b07785b898d75f373ec4f821037d1a98c29e481
r3c0n <pentestwizard@gmail.com>
2016-11-13 22:36:36 -0500

    install

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 file="key.txt"
+# yes I know, easy shell script, but it gets the job done without python and external-
+# python modules.
+#
 # ask for shodan API key
 echo "Shodan key: "
 read key
