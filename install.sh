0b46dbbb72f9fda47633e4af4682efaf52bcdc09
r3c0n <pentestwizard@gmail.com>
2016-11-24 20:40:24 -0500

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-file="key.txt"
-# yes I know, easy shell script, but it gets the job done without python and external-
-# python modules.
-#
-# ask for shodan API key
-echo "Shodan key: "
-read key
-echo $key > $file
